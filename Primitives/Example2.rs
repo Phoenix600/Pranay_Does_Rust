4bc766f0763f25eaa9b2c2df13d272c324a72055
phoenix600 <pranayramteke613@gmail.com>
2024-08-22 23:02:34 -0700

    Added The Code For Unsigned Integer

--- Primitives/Example2.rs
+++ Primitives/Example2.rs
@@ -18,4 +18,19 @@ fn main()
     println!("128Bit Signed Integer {:?}",x4);
 
 
+    let u1:u8 = 126;
+    println!("8Bit Unsigned Integer {0}",u1);
+
+    let u2:u16 = 2800;
+    println!("16Bit Unsigned Integer {0}",u2);
+
+    let u3:u32 = 39743; 
+    println!("32Bit Unsigned Integer {0}",u3);
+
+    let u4:u64 = 23474644334u64;
+    println!("64Bit Unsigned Integer {:?}",u4);
+
+    let u5:u128 = 3427462347862347234823434u128;
+    println!("128Bit Unsigned Integer {:?}",u5);
+
 }
